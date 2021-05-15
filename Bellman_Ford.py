e8075beb1813904514e1e7a38d3cca70337c7c24
ksanchezalva <kimberly.sanchez@student.uclouvain.be>
2021-05-15 15:44:54 +0200

    traduction

--- Bellman_Ford.py
+++ Bellman_Ford.py
@@ -2,20 +2,20 @@ import numpy as np
 import math
 
 def Bellman_Ford(input_matrix: np.array):
-    distance_matrix = np.copy(input_matrix) #copy de l'input
-    n = distance_matrix.shape[0] # n -> nombre des aretes du graphe
+    distance_matrix = np.copy(input_matrix) #copy of the input
+    n = distance_matrix.shape[0] # n -> number of edges in a graph
     """
-    pre 'distance_matrix' est la matrice de distances de taille nxn ou n est le nombre des aretes du graphe
-    post renvoie la matrice des plus courts chemins
-    post si la matrice contiene des clycles negatifs renvoie "Cycle negatif"
+    pre 'distance_matrix' matrix of distances with size nXn where n is the number of nedges in the graph
+    post returns the shortest path matrix
+    post if the matrix contains negative clycles returns the mensaje "Negative Clycle"
     """
     for k in range(n):
         for i in range(n):
             for j in range(n):
                 w = distance_matrix[k][j]
-                if distance_matrix[i][j] > distance_matrix[i][k] + w: #etape de relaxation
-                    distance_matrix[i][j] = distance_matrix[i][k]+ w # mettre a jour les valeurs 
-    # Verification des cycles negatives
+                if distance_matrix[i][j] > distance_matrix[i][k] + w: #relaxation step
+                    distance_matrix[i][j] = distance_matrix[i][k]+ w # update the values
+    # verification of negative cycles 
     for i in range(n):
         for j in range(n-1): 
             w = distance_matrix[i][j]       
